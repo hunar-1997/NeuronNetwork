d5341919e0fb3e162d16b93a8acb3a8a295929ad
hunar1997 <hbkurd@gmail.com>
2016-11-19 11:59:50 +0300

    i took the second step towards dinamic code

--- Traning.py
+++ Traning.py
@@ -1,6 +1,6 @@
 import math
 
-seed = 4
+seed = 1
 def rand(x):
     global seed
     seed *= 17373172882
@@ -58,27 +58,35 @@ def adj(a,b,c):
     return a
 
 # Traning data
-data=[[0,0,1],
+data=[[0,0,0],
+      [0,0,1],
+      [0,1,0],
+      [1,0,0],
       [0,1,1],
       [1,0,1],
+      [1,1,0],
       [1,1,1]]
 
 # Desired output
-output=[[0],
-        [1],
-        [1],
-        [0]]
+output=[[0,0],
+        [0,1],
+        [0,1],
+        [0,1],
+        [1,0],
+        [1,0],
+        [1,0],
+        [1,1]]
 
 # inputs , (hidden layers,), output
-layers = [ len(data[0]), 4, 2, len(output[0]) ]
+layers = [ len(data[0]), 3, 3, len(output[0]) ]
 
-ll = []
+cl = []
 for i in range(len(layers)-1):
-    ll.append( shuffle(layers[i+1], layers[i]) )
+    cl.append( shuffle(layers[i+1], layers[i]) )
 
-con1 = ll[0]
-con2 = ll[1]
-con3 = ll[2]
+con1 = cl[0]
+con2 = cl[1]
+con3 = cl[2]
 
 errList=[]
 for i in range(len(data)):
@@ -87,15 +95,22 @@ for i in range(len(data)):
         errList[i].append(100)
 
 count=0
-for j in range(60000):
+for j in range(100000):
     # Random position for the traning data
     rData = count % len(data)
     count += 1
     
-    # Get the values of the layers
-    l1 = dot( con1, data[rData] )
-    l2 = dot( con2, l1 )
-    l3 = dot( con3, l2 )
+    # Calculating layer by layer
+    ll = []
+    for i in range(len(layers)-1):
+        if(i==0):
+            ll.append( dot(cl[0], data[rData]) )
+        else:
+            ll.append( dot(cl[i], ll[i-1]) )
+    
+    l1 = ll[0]
+    l2 = ll[1]
+    l3 = ll[2]
     
     l3_err = sub( output[rData], l3 )
     l3_delta = mulD( l3_err, l3 )
@@ -119,6 +134,7 @@ for j in range(60000):
             for k in i:
                 t += k
         print("Error:", t/(len(errList)*len(errList[0])), "%")
+
 print("-"*10)
 print("result after train:")
 for i in data:
@@ -126,10 +142,11 @@ for i in data:
     l2 = dot(con2, l1)
     l3 = dot(con3, l2)
     for j in l3:
-        print("{:.3f}".format(j),end="  ")
+        print("{:.0f}".format(j),end="  ")
     print()
-if True and False:
+if True:
     print("The brain data:")
-    print("con1 =", con1)
-    print("con2 =", con2)
-    print("con3 =", con3)
+    for i in range(len(ll)):
+        print("con"+str(i+1)+" =", ll[i])
+
+
